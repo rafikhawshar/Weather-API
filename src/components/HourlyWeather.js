9fee250f690ce9ff4f380d55a4d8b0f15b16f8c2
rafikxhawshar <rafikhawshar@gmail.com>
2023-09-15 10:26:51 +0300

    Weather API: Breaking the HTML in App.js into React components

--- src/components/HourlyWeather.js
+++ src/components/HourlyWeather.js
@@ -1,49 +1,13 @@
 import React from "react";
 
-import Search from "./components/Search";
+import clear from "../img/weather-icons/clear.svg";
+import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
 
-import { CurrentWeather, WeatherItem } from "./components/WeatherItem"
-import fakeWeatherData from "./fakeWeatherData.json";
-import clear from "./img/weather-icons/clear.svg";
-import cloudy from "./img/weather-icons/cloudy.svg"
-import partlycloudy from "./img/weather-icons/partlycloudy.svg"
-import mostlycloudy from "./img/weather-icons/mostlycloudy.svg"
-import Nav from "./components/Nav";
-import WeatherNow from "./components/WeatherNow";
-import HourlyWeather from "./components/HourlyWeather";
-import FakeWeather from "./fakeWeatherData.json";
-import "./App.css";
-export default App;
+function HourlyWeather() {
+  
 
-function App() {
   return (
-    <div className="app">
-      <Nav/>
-      <main>
-        <WeatherNow/>
-        <HourlyWeather/>
-      </main>
-      {/* <div id="wrapper">
-        <header>
-
-          <input type="text" name="country" id="country" placeholder="Type in a City Name" />
-          <input class="button" type="submit" value="FIND WEATHER"></input>
-        </header>
-
-        <main>
-
-          <section class="currentweather">
-            <img class="bodyimg" src={mostlycloudy} alt="weatherimg" />
-            <div id="title">overcast Clouds</div>
-            <p className="temp"><span className="weatherTemp">Temperature</span>  10&deg;C to 11&deg;C </p>
-
-            <p className="climatemetrics">
-              <span className="a">Humidity </span><span>78%</span><span className="a">Pressure </span>1<span>00.48</span>
-            </p>
-
-
-          </section>
-          <section class="dayweather">
+    <section class="dayweather">
             <article class="timedata">
               <time class="time" datetime="hh:mm">3:00</time>
               <img class="weatherimg" src={mostlycloudy} alt="weatherimg" placeholder="http://placekitten.com/g/200/300" />
@@ -89,14 +53,7 @@ function App() {
 
 
           </section>
-        </main>
-      </div> */}
-
-    </div>
-  )
+  );
 }
 
-
-
-
-
+export default HourlyWeather;
